536b8e44f1af8d2fe527c09926d1c7f3cbc04345
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-07-19 01:08:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -76,9 +76,6 @@ Client.on("guildMemberRemove", member =>
 
 function serverEvent (guild, title, user, time, message, edit) {
 
-	var msg = `${guild}\n${title}\n${user}\n${time}\n${message}\n${edit}`;
-	Client.guilds.cache.get("704494659400892458").channels.cache.get("734134248017231972").send(msg).then().catch();
-
 	let logChannel = Tools.settings.read(guild, "logchannel");
 	let channel = guild.channels.cache.get(logChannel);
 
