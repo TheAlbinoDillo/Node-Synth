162887fa55a77fe96391304c2252560e6505bf42
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-07-25 15:28:11 -0400

    added fg.frombin

---
+++ files/commands/tools/from_bin.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function run (message, args)
+{
+	return parseInt(args.full, 2).toString();
+}
+
+module.exports =
+{
+	name: "From Binary",
+	description: "Turn binary into a decimal number!",
+	calls:
+	[
+		"frombin"
+	],
+	runFunction: run,
+	usage: ["binary number"]
+};
