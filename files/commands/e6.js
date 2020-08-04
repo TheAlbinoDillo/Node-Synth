98a45d6a97843e936290bcbfc60d409f76a08283
TheAlbinoDillo <65730701+TheAlbinoDillo@users.noreply.github.com>
2020-08-04 09:50:22 -0400

    Create e6.js

---
+++ files/commands/e6.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+function run(){
+fetch​(​'https://e621.net/posts.json',​ 
+{​
+    method​: ​'get',​
+    headers: 
+{​ 
+    'User-Agent': ​"Node-Synth/1.0 (by thealbinoarmadillo on e621)" ​}​,​
+​}​)​​.​then​(​res​ ​=>​ ​console.log(res) )​;}
+
+module.exports = {runFunction:run, permissions:["BOT_OWNER"], calls:["e6"]};
