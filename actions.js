698ce0a1b8f0c46501deb7a58dc82b50507dcd29
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-08-21 10:02:34 -0400

    Added message sending reactions

--- actions.js
+++ actions.js
@@ -5,11 +5,10 @@ const index = root_require("index.js");
 
 class BotActions
 {
-	static async send (options)
+	static async send (options, content)
 	{	
 		let channel = options.channel;
 		let message = options.message;
-		let content = options.send_content;
 
 		if (!(channel instanceof discord.TextChannel) )
 		{
@@ -36,7 +35,7 @@ class BotActions
 			console.error(`Error sending message:\n${error.message}\n`);
 			if (message)
 			{
-				errorReact(message, "‼️", `\`${message.content}\`\nMessage error: ${error.message}`);
+				this.react_say(message, "‼️", `\`${message.content}\`\nMessage error: ${error.message}`);
 			}
 		});
 		return sent;
@@ -47,6 +46,37 @@ class BotActions
 		message.react(emote).catch(error =>
 		{
 			console.error(`Failed to react to message:\n\t${error.message}\n`);
+		});
+	}
+
+	static async react_say (message, emote, content)
+	{
+		await this.react(message, emote);
+
+		let collector = message.createReactionCollector( (reaction, user) =>
+		{
+			if (reaction.emoji.name !== emote) return false;
+			if (user !== message.author) return false;
+			return true;
+
+		}, 120000);
+
+		collector.on("collect", (reaction) =>
+		{
+			collector.stop("complete");
+
+			let options =
+			{
+				message: message,
+				channel: message.channel
+			};
+
+			this.send(options, content);
+		});
+
+		collector.on("end", (collected, reason) =>
+		{
+			if (reason === "time") this.react(message, "⏰");
 		});
 	}
 }
