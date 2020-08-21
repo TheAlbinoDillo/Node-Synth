d9d27e9afcb3a0a4d64faba59aa761434b100f1c
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-08-21 10:03:22 -0400

    Added error reactions when running command

--- runtime/events/message.js
+++ runtime/events/message.js
@@ -2,6 +2,7 @@
 
 const commands = root_require("commands.js");
 const actions = root_require("actions.js");
+const index = root_require("index.js");
 
 const command_test = /(?<prefix>^fg(?:\. |[^a-z0-9?]))(?<call>[^\s]+) ?(?<options>.+)*/gi;
 const options_test = /\w+|(?:"[^"]*"|`[^`]*`|'[^']*')/g;
@@ -12,6 +13,7 @@ this.run = (message) =>
 	let content = message.content;
 	let guild = message.guild;
 	let author = message.author;
+	let member = message.member;
 
 	//Fail if message has no guild
 	//This usually means it's a DM message
@@ -49,28 +51,40 @@ this.run = (message) =>
 	let selected_command = commands.command_list.find(call);
 
 	//Fail if this command doesn't exist
-	if (!selected_command) return;
+	if (!selected_command)
+	{
+		let say = `\`${call}\` is not a command.`;
+		actions.react_say(message, "❓", say);	
+		return;
+	}
 
 	//Test if user has permission to use this command
-	
-	
-	//Try to run command
-	try
+	if (selected_command.perms.includes("BOT_OWNER") )
 	{
-		selected_command.run(
+		if (member.id !== index.client_settings.owner)
 		{
-			message: message,
-			guild: message.guild,
-			channel: message.channel
-		});	
+			let say = `Only the bot owner can use \`${selected_command.name}\`.`;
+			actions.react_say(message, "⛔", say);
+			return;
+		}
 	}
-	catch(error)
+	else if (!member.permissions.has(selected_command.perms) )
 	{
-		console.error(error.stack);
+		let say = `${member} does not have permission to use \`${selected_command.name}\`.`;
+		actions.react_say(message, "⛔", say);
+		return;
 	}
 
-	//Make a system to find commands effciently
-	//Maybe a CommandList class with search options
+	//Try to run command
+	selected_command.run(
+	{
+		message: message,
+		guild: message.guild,
+		channel: message.channel
 
-        //Make the events script handle promises
+	}).catch( (error) =>
+	{
+		let say = `\`${selected_command.name}\`\n${error.message}`;
+		actions.react_say(message, "⁉️", say);
+	});
 };
