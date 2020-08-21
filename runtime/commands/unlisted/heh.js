c70a293c02dce63cfd2d399c27a0bfb12df1a399
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-08-21 12:59:02 -0400

    Update heh.js

--- runtime/commands/unlisted/heh.js
+++ runtime/commands/unlisted/heh.js
@@ -1,10 +1,13 @@
 "use strict";
 
+const actions = root_require("actions.js");
+
 module.exports =
 {
 	name: "heh",
+	perms: ["MANAGE_MESSAGES"],
 	run: async (options) =>
 	{
-		return "heh";
+		actions.send(options, "heh");
 	}
 }
