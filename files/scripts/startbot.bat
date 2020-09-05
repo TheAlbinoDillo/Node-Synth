e17b7b554deebf7a6d7482a3f9656f7edac69977
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-09-05 11:28:25 -0400

    Delete old files

--- files/scripts/startbot.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:A
-cd C:\Users\mojo4\Documents\GitHub\Bot-FurGun
-echo Starting bot...
-node .
-if %errorlevel%==2 (echo Bot Disconnected)
-if %errorlevel%==5 (GOTO A) else (pause & GOTO A)
-pause
