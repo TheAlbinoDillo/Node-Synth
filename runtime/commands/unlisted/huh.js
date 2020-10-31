b98d7afa9b279f2f54aa858a6c23a6145f07251a
TheAlbinoDillo <65730701+TheAlbinoDillo@users.noreply.github.com>
2020-10-31 16:39:51 -0400

    Create huh.js

---
+++ runtime/commands/unlisted/huh.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+const actions = script_require("actions.js");
+
+module.exports =
+{
+	name: "Huh?",
+calls: ["huh"],
+	perms: [],
+	run: async (options) =>
+	{
+		actions.send(options, "huh");
+    options.message.delete().catch();       
+	}
+}
