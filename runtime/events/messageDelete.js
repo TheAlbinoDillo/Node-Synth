4646fc6f7437d3a2fddeec5f5bc629a7e04e1604
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2020-10-31 15:42:22 -0400

    Create messageDelete.js

---
+++ runtime/events/messageDelete.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const Actions = script_require("actions.js");
+
+const name = "message-deleted-node-synth-log";
+
+async function run (message)
+{
+	//Ignore messages from bots
+	if (message.author.bot) return;
+
+	//Get channel
+	let channels = message.guild.channels.cache;
+	let logChannel = channels.filter( (c) =>
+	{
+		return c.name === name;
+
+	}).array();
+
+	if (logChannel.length == 0) return;
+
+	let embed =
+	{
+		"title": "Message Deleted",
+		"color": 14093067,
+		"thumbnail":
+		{
+			"url": message.author.avatarURL()
+		},
+		"fields":
+		[
+			{
+				"name": "User:",
+				"value": `${message.author}`,
+				"inline": true
+			},
+			{
+				"name": "Channel:",
+				"value": `${message.channel}`,
+				"inline": true
+			},
+			{
+				"name": "Message:",
+				"value": `${message.content || "`No message content.`"}`
+			}
+		]
+	};
+
+	Actions.send({channel: logChannel[0]}, {embed: embed});
+}
+
+module.exports =
+{
+	run: run
+};
