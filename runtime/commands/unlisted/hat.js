8f1eabdf468d845859a731f4a49a26556427d8bb
TheAlbinoDillo <mojo4fl@gmail.com>
2020-11-28 18:53:00 -0500

    Create hat.js

---
+++ runtime/commands/unlisted/hat.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const actions = script_require("actions.js");
+
+module.exports =
+{
+	name: "Hat",
+	calls: ["hat"],
+	perms: [],
+	run: async (options) =>
+	{
+		actions.send(options, `http://choohy.com:5000/${options.user.id}`);
+	}
+}
