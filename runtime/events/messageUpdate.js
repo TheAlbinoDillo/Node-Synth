3952dcac249f5dc00857ac749cd9130cf9d08f13
TheAlbinoDillo <mojo4fl@gmail.com>
2021-02-03 12:43:21 -0500

    DM edit fix
    Now ignores edited messages coming from DM channels. Also added more comments for readability.

--- runtime/events/messageUpdate.js
+++ runtime/events/messageUpdate.js
@@ -4,14 +4,19 @@ const actions = script_require("actions.js");
 const index = root_require("index.js");
 const diff = require("diff");
 
+// Name of channel the log will be sent
 const name = "message-edited-node-synth-log";
 
 async function run (oldMessage, newMessage)
 {
-	//Ignore messages from bots
+	// Ignore if message has no guild
+	// This usually means it's a DM message
+	if (!oldMessage.guild) return;
+
+	// Ignore edited messages from bots
 	if (newMessage.author.bot) return;
 
-	//Get channel
+	// Get channel of edited message
 	let channels = newMessage.guild.channels.cache;
 	let logChannel = channels.filter( (c) =>
 	{
@@ -19,12 +24,13 @@ async function run (oldMessage, newMessage)
 
 	}).array();
 
+	// Do nothing if a log channel has not been made
 	if (logChannel.length == 0) return;
 
+	// Get difference of messages
 	let textDiff = diff.diffWords(oldMessage.content, newMessage.content);
 
-	//console.log(textDiff);
-
+	// From difference string for embed
 	let diffString = "";
 	for (let i = 0; i < textDiff.length; i++)
 	{
@@ -34,6 +40,7 @@ async function run (oldMessage, newMessage)
 
 	diffString = diffString.replace(/\`/g, "\\`");
 
+	// Create embed
 	let embed =
 	{
 		"title": "Message edited",
@@ -63,12 +70,13 @@ async function run (oldMessage, newMessage)
 				"value": `${newMessage.content}`
 			},
 			{
-				"name": "Diff:",
+				"name": "Difference:",
 				"value": "```diff\n" + diffString + "\n```"
 			},
 		]
 	};
 
+	// Send embed to the log channel
 	actions.send({channel: logChannel[0]}, {embed: embed});
 }
 
