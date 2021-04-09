afb0c239befe1f3b1e8640d223ee54d97f973fea
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2021-04-08 22:15:19 -0400

    Changed comment spacing

--- runtime/events/message.js
+++ runtime/events/message.js
@@ -2,13 +2,13 @@
 
 const index = require("../../index.js");
 
-//RegEx to test for the prefix call and command
+// RegEx to test for the prefix call and command
 const command_test = /(?<prefix>^fg(?:\. |[^a-z0-9?]))(?<call>[^\s<]+) ?(?<options>.+)*/gi;
 
-//RegEx to seperate the options out
+// RegEx to seperate the options out
 const options_test = /[^\s]+|(?:"[^"]*"|`[^`]*`|'[^']*')/g;
 
-//Test if user's id is the bot's owner
+// Test if user's id is the bot's owner
 function isOwner (user)
 {
 	return user.id == process.env.CLIENT_OWNER;
@@ -16,49 +16,49 @@ function isOwner (user)
 
 async function run (message)
 {
-	//Breakout message object properties
+	// Breakout message object properties
 	let content = message.content;
 	let guild = message.guild;
 	let author = message.author;
 	let member = message.member;
 
-	//Fail if message has no guild
-	//This usually means it's a DM message
+	// Fail if message has no guild
+	// This usually means it's a DM message
 	if (!guild) return;
 
-	//Fail if the prefix is being ran by a bot
+	// Fail if the prefix is being ran by a bot
 	if (author.bot) return;
 
-	//reset command_test RegEx pointer
+	// reset command_test RegEx pointer
 	command_test.lastIndex = 0;
-	//Run the command_test RegEx to get compenents of a
-	//command (if there are any to get)
+	// Run the command_test RegEx to get compenents of a
+	// command (if there are any to get)
 	let command_msg = command_test.exec(content);
 
-	//Fail if no command structured matches were found
+	// Fail if no command structured matches were found
 	if (!command_msg) return;
 
-	//Breakout command_msg object properties
+	// Breakout command_msg object properties
 	let groups = command_msg.groups;
 
-	//Breakout groups object properties
+	// Breakout groups object properties
 	let prefix = groups.prefix;
 	let call = groups.call.toLowerCase();
 	let cmd_options = groups.options;
 	let cmd_args;
 
-	//If there are options with the command
-	//replace them with a structured array
+	// If there are options with the command
+	// replace them with a structured array
 	if (cmd_options)
 	{
 		let matches = cmd_options.match(options_test);
 		cmd_args = matches;
 	}
 
-	//Search for command
+	// Search for command
 	let selectedCommand = VarCommandList[VarCommandCalls[call] ];
 
-	//Fail if this command doesn't exist
+	// Fail if this command doesn't exist
 	if (!selectedCommand)
 	{
 		let say = `\`${call}\` is not a command.`;
@@ -66,7 +66,7 @@ async function run (message)
 		return;
 	}
 
-	//Test if this command is NSFW
+	// Test if this command is NSFW
 	if (selectedCommand.nsfw && !message.channel.nsfw)
 	{
 		let msg = `\`${selectedCommand.name}\` is only allowed in NSFW channels.`;
@@ -74,13 +74,13 @@ async function run (message)
 		return;
 	}
 
-	//Test if user has permission to use this command
+	// Test if user has permission to use this command
 	if (selectedCommand.perms.includes("BOT_OWNER") )
 	{
-		//Test if the user is this bot's owner
+		// Test if the user is this bot's owner
 		if (!isOwner(member) )
 		{
-			//Fail if the user is not the bot's owner
+			// Fail if the user is not the bot's owner
 			let say = `Only the bot owner can use \`${selectedCommand.name}\`.`;
 			BotActions.react_say(message, "⛔", say);
 			return;
@@ -88,13 +88,13 @@ async function run (message)
 	}
 	else if (!member.permissions.has(selectedCommand.perms) && !isOwner(member) )
 	{
-		//Fail if the user doesn't have the needed permissions
+		// Fail if the user doesn't have the needed permissions
 		let say = `${member} does not have permission to use \`${selectedCommand.name}\`.`;
 		BotActions.react_say(message, "⛔", say);
 		return;
 	}
 
-	//Create an options object to send to the command
+	// Create an options object to send to the command
 	let options =
 	{
 		message: message,
@@ -107,7 +107,7 @@ async function run (message)
 		client: message.client
 	};
 
-	//Try to run command
+	// Try to run command
 	selectedCommand.run(options).catch( (error) =>
 	{
 		let say = `\`${selectedCommand.name}\`\n${error.stack}`;
