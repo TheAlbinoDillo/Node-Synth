f0da47b43082883b25109b28a6ef0877d1be15aa
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2021-04-09 21:44:00 -0400

    Modified invite to support variability

--- runtime/commands/tools/invite.js
+++ runtime/commands/tools/invite.js
@@ -1,12 +1,22 @@
 "use strict";
 
+const baseurl = "https://discord.com/oauth2/authorize";
+const perms = 67497024;
+
+/*const query =
+{
+	client_id: VarClient.user.id,
+	scope: "bot",
+	permissions: 67497024
+};*/
+
 async function run (options)
 {
 	let embed =
 	{
 		"title": ":link: Invite me to your server! :link:",
 		"color": 3169024,
-		"url": "https://discord.com/oauth2/authorize?client_id=662825806967472128&scope=bot&permissions=8"
+		"url": `${baseurl}?client_id=${VarClient.user.id}&scope=bot&permissions=${perms}`
 	};
 
 	BotActions.send(options, {embed: embed});
