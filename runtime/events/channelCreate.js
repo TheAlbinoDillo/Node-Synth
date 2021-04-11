f35c61f9a81136a63426012c6a690ea1721e1ab3
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2021-04-11 13:13:30 -0400

    Updated createChannel message

--- runtime/events/channelCreate.js
+++ runtime/events/channelCreate.js
@@ -9,7 +9,9 @@ async function run (channel)
 
 	let message =
 	"Just a heads up that I can read this channel, if you don't want that, make sure " +
-	"to turn off my ability to read messages in this channel's permission settings."
+	"to turn off my ability to read messages in this channel's permission settings.\n\n" +
+	"Note: This will not work if I still have Admin permissions on your server. Run `" +
+	"fg reinvite` to see how and why you should remove them."
 
 	BotTools.send(options, message, true);
 }
