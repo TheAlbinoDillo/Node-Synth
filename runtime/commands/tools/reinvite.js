492fbdf95ca3a4deb4a995b3afdc76076345b92e
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2021-04-11 13:11:41 -0400

    Updated reinvite message

--- runtime/commands/tools/reinvite.js
+++ runtime/commands/tools/reinvite.js
@@ -14,9 +14,14 @@ async function run (options)
 				"name": "Why?",
 				"value": "Having Admin status is unnecessary for completing my commands. " +
 				"It also prevents you from being able block me from channels you may not " +
-				"want me to read.\n\nAlso, under the slim possiblity that anyone hacks " +
-				"into my code, they will gain full control of your server via my " +
+				"want me to read.\n\nAlso, under the slim possiblity that anyone gets " +
+				"my bot token, they could gain full control of your server via my " +
 				"abilities. This is, however, just a worst case scenario."
+			},
+			{
+				"name": "Alternatively",
+				"value": "You may also manually review and change my permissions by editing" +
+				" the `NodeSynthBot` role in your server role settings."
 			}
 		]
 	};
