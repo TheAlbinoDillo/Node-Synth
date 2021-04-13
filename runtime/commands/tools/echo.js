ed2c59b76e5e80787ac7b449d78f7283afc85c1b
TheAlbinoDillo <thealbinoarmadillo@gmail.com>
2021-04-13 14:55:08 -0400

    Fixed crash by making this async

--- runtime/commands/tools/echo.js
+++ runtime/commands/tools/echo.js
@@ -1,9 +1,14 @@
 "use strict";
 
-function run (options)
+async function run (options)
 {
 	BotActions.send(options, options.full);
-	options.message.delete().catch();
+
+	let myMember = options.guild.members.cache.get(VarClient.user.id);
+	let hasPerm = myMember.permissions.has("MANAGE_MESSAGES");
+	if (hasPerm)
+		options.message.delete().catch();
+	// todo: BotTools delete message function
 }
 
 module.exports =
